2991115b60a5da32ed235a11f680717ef5d2be0a
Henrique Alves <henryworkdev.ti@gmail.com>
2023-05-18 14:54:57 -0300

    evento botão modo noturno

--- cronometro/index.js
+++ cronometro/index.js
@@ -1,11 +1,28 @@
+//elementos do DOM
+const cronometro = document.querySelector("#cronometro");
+const btn_modo_noturno = document.querySelector("#btn-modo-noturno");
+const container = document.querySelector("#container");
+const icone_ligar = document.querySelector("#icone-ligar");
+const icone_desligar = document.querySelector("#icone-desligar");
+
 function atualizaHora(){
     const data = new Date();
     const horas = data.getHours();
     const minutos = data.getMinutes();
     const segundos = data.getSeconds();
     const hora_formatada = `${horas}:${minutos}:${segundos}`; 
-    const cronometro = document.querySelector("#cronometro");
     cronometro.children[0].innerHTML = hora_formatada;
 }
 
 setInterval(() => atualizaHora(), 1000);
+
+//função do botão
+btn_modo_noturno.onclick = function(){
+   //1-mudar a cor de fundo de toda a página 
+   //2-mudar a cor de texto de toda a página
+   //3-mudar o ícone do botão
+   container.classList.toggle("modo-noturno");
+   icone_ligar.classList.toggle("invisivel");
+   icone_desligar.classList.toggle("invisivel");
+}
+
